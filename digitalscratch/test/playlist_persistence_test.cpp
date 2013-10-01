4c4239a0dc88d94bad6ea3730662d748b1138a50
julienr <julien.rosener@digital-scratch.org>
2013-10-01 22:43:08 +0200

    Fixed playlist test.

--- digitalscratch/test/playlist_persistence_test.cpp
+++ digitalscratch/test/playlist_persistence_test.cpp
@@ -4,7 +4,26 @@
 #include <iostream>
 
 #define DATA_DIR          "./test/data/"
-#define DATA_PLAYLIST_M3U "playlist.m3u"
+#define PLAYLIST_M3U_FILE "playlist.m3u"
+#define PLAYLIST_M3U "#EXTM3U\n"\
+                     "#EXTINF:2316,\n"\
+                     "# full path\n"\
+                     "<DATA_DIR>/track_1.mp3\n"\
+                     "#EXTINF:2316, -\n"\
+                     "# relative path\n"\
+                     "track_2.mp3\n"\
+                     "#EXTINF:2316, -\n"\
+                     "# duplicate path\n"\
+                     "track_2.mp3\n"\
+                     "#EXTINF:2316, -\n"\
+                     "# file does not exists\n"\
+                     "file://<DATA_DIR>/track_does_not_exists.mp3\n"\
+                     "#EXTINF:2316, -\n"\
+                     "# uri style path\n"\
+                     "file://<DATA_DIR>/track_%C3%A9%C3%A8%C3%A0%40%C3%B9%26_3.mp3\n"\
+                     "#EXTINF:2316, -\n"\
+                     "# special chars\n"\
+                     "track_éèà@ù&_4.mp3"
 
 Playlist_persistence_Test::Playlist_persistence_Test()
 {
@@ -13,12 +32,25 @@ Playlist_persistence_Test::Playlist_persistence_Test()
 
 void Playlist_persistence_Test::initTestCase()
 {
+    // Init playlist and persistence process.
     this->playlist         = new Playlist("playlist");
     this->playlist_persist = new Playlist_persistence();
+
+    // Create a test playlist file.
+    QString m3u(PLAYLIST_M3U);
+    m3u.replace("<DATA_DIR>", QDir(DATA_DIR).absolutePath());
+    QFile m3u_file(QDir(DATA_DIR).filePath(PLAYLIST_M3U_FILE));
+    if (m3u_file.open(QIODevice::WriteOnly | QIODevice::Text) == true)
+    {
+         QTextStream m3u_stream(&m3u_file);
+         m3u_stream << m3u.toUtf8();
+         m3u_file.close();
+    }
 }
 
 void Playlist_persistence_Test::cleanupTestCase()
 {
+    // Cleanup.
     delete this->playlist;
     delete this->playlist_persist;
 }
@@ -26,7 +58,7 @@ void Playlist_persistence_Test::cleanupTestCase()
 void Playlist_persistence_Test::testCaseReadM3u()
 {
     // Read playlist.
-    QString playlist_fullfilename = QDir(DATA_DIR).filePath(DATA_PLAYLIST_M3U);
+    QString playlist_fullfilename = QDir(DATA_DIR).filePath(PLAYLIST_M3U_FILE);
     QVERIFY2(this->playlist_persist->read_m3u(playlist_fullfilename, this->playlist) == true, "Read M3U");
 
     // Check tracklist.
