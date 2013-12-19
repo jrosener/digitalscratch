9ed630cfdcdcefe4ad1f42f3eccc6b17608daba4
julienr <julien.rosener@digital-scratch.org>
2013-12-20 00:32:22 +0100

    Added rebuild option to ubuntu packaging script.

--- digitalscratch/dist/ubuntu/generate_digitalscratch_deb.sh
+++ digitalscratch/dist/ubuntu/generate_digitalscratch_deb.sh
@@ -15,15 +15,23 @@ function check_error {
 # Usage
 function usage {
     echo ""
-    echo "Usage: generate_digitalscratch_deb.sh [version_number]"
+    echo "Usage: generate_digitalscratch_deb.sh [ppa_type] <--rebuild>"
     echo ""
-    echo "    [ppa_type]       'test' (for the test PPA url) or 'prod'"
+    echo " Mandatory args:"
+    echo "    [ppa_type]  'test' (for the test PPA url) or 'prod'"
+    echo ""
+    echo " Optional args:"
+    echo "    <--rebuild> <path_to_orig.tar.gz> do not use the actual source code but the defined .orig.tar.gz"
     echo ""
     exit
 }
 
 # Check parameters
-if [ $# -ne 1 ]; then
+if [ $# < 1 ]; then
+    usage
+fi
+
+if [ $# == 2 ]; then
     usage
 fi
 
@@ -51,13 +59,13 @@ echo ""
 
 echo "*************************** Prepare environment *************************"
 # Main vars
-VERSIONPACKAGE=$VERSION-0ubuntu1
 WORKINGPATH=$HOME/digitalscratch_$VERSION-make_package
 DEBPATH=$WORKINGPATH/deb
-SOURCEDIR=digitalscratch_source
-TARPACK=digitalscratch_$VERSION.orig.tar.gz
+SOURCEDIR_ORIG=digitalscratch_source
 ORIGDIR=$(pwd)
 DISTRIB=$(lsb_release -cs)
+VERSIONPACKAGE=$VERSION-1ppa1~${DISTRIB}1
+TARPACK=digitalscratch_$VERSION.orig.tar.gz
 PPAURL=http://ppa.launchpad.net
 export DEBEMAIL=julien.rosener@digital-scratch.org
 export DEBFULLNAME="Julien Rosener"
@@ -70,18 +78,32 @@ check_error
 echo ""
 echo ""
 
-echo "**************************** Copy source code ***************************"
+echo "**************************** Get source code ***************************"
 git checkout debian/changelog
 check_error
 cd ../../
 git archive --format zip --output $WORKINGPATH/archive.zip master
-unzip $WORKINGPATH/archive.zip -d $WORKINGPATH/$SOURCEDIR
+unzip $WORKINGPATH/archive.zip -d $WORKINGPATH/$SOURCEDIR_ORIG
+rm -v -rf $WORKINGPATH/$SOURCEDIR_ORIG/dist
 check_error
 echo ""
 echo ""
 
+if [[ $2 == --rebuild ]] ; then
+    echo "************************* Copy old source package ***********************"
+    cp -v $3 $WORKINGPATH
+else
+    echo "******************** Compress orig source directory *********************"
+    ORIGDIR=$(pwd)
+    cd $WORKINGPATH
+    tar cvzf $TARPACK $SOURCEDIR_ORIG
+    cd $ORIGDIR
+fi
+echo ""
+echo ""
+
 echo "**************************** Install debian/ folder ***************************"
-cp -r $WORKINGPATH/$SOURCEDIR/dist/ubuntu/debian $WORKINGPATH/$SOURCEDIR/
+cp -v -r dist/ubuntu/debian $WORKINGPATH/$SOURCEDIR_ORIG/
 check_error
 echo ""
 echo ""
@@ -89,24 +111,18 @@ echo ""
 echo "************************* Update changelog ******************************"
 cd dist/ubuntu/debian
 ORIGDIR=$(pwd)
-cd $WORKINGPATH/$SOURCEDIR
+cd $WORKINGPATH/$SOURCEDIR_ORIG
 debchange --newversion $VERSIONPACKAGE --distribution $DISTRIB
 check_error
-cat $WORKINGPATH/$SOURCEDIR/debian/changelog
-cp $WORKINGPATH/$SOURCEDIR/debian/changelog $ORIGDIR
+cat $WORKINGPATH/$SOURCEDIR_ORIG/debian/changelog
+cp $WORKINGPATH/$SOURCEDIR_ORIG/debian/changelog $ORIGDIR
 check_error
-echo ""
-echo ""
-
-echo "************************* Compress source directory *********************"
-cd $WORKINGPATH
-tar cvzf $TARPACK $SOURCEDIR/
 echo ""
 echo ""
 
 echo "***************************** Create Linux base *************************"
 export BUILDUSERID=$USER
-cd $WORKINGPATH/$SOURCEDIR
+cd $WORKINGPATH/$SOURCEDIR_ORIG
 export OTHERMIRROR="deb $PPAURL/$PPAPATH/ubuntu $DISTRIB main"
 if [ ! -f ~/pbuilder/$DISTRIB-base.tgz ]
 then
@@ -116,7 +132,7 @@ pbuilder-dist $DISTRIB update
 echo ""
 echo ""
 
-echo "************Parse debian/ config file and create source.changes *********"
+echo "************ Parse debian/ config file and create source.changes *********"
 debuild -S -sa
 check_error
 cd ../
