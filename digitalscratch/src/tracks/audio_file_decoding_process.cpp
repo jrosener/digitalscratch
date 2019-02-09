a8bd7433e05a6c85f51ed548bedbe8d997b5977e
Julien Rosener <julien.rosener@digital-scratch.org>
2019-02-09 11:27:10 +0100

    travis CI: testing Ubuntu Xenial as build env

--- digitalscratch/src/tracks/audio_file_decoding_process.cpp
+++ digitalscratch/src/tracks/audio_file_decoding_process.cpp
@@ -274,8 +274,8 @@ Audio_file_decoding_process::decode()
         av_opt_set_int(swr, "out_channel_layout", codec_context->channel_layout,  0);
         av_opt_set_int(swr, "in_sample_rate",     codec_context->sample_rate, 0);
         av_opt_set_int(swr, "out_sample_rate",    codec_context->sample_rate, 0);
-        //av_opt_set_sample_fmt(swr, "in_sample_fmt",  AV_SAMPLE_FMT_FLTP, 0);
-        //av_opt_set_sample_fmt(swr, "out_sample_fmt", AV_SAMPLE_FMT_S16,  0);
+        av_opt_set_sample_fmt(swr, "in_sample_fmt",  AV_SAMPLE_FMT_FLTP, 0);
+        av_opt_set_sample_fmt(swr, "out_sample_fmt", AV_SAMPLE_FMT_S16,  0);
         swr_init(swr);
     }
 
